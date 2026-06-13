4e2478aff862e155392ee042a00881f461a2b42c
tanzeelalam <tanzeelalam@gmail.com>
2026-06-13 11:50:50 +0530

    Update to handle browser on linux VM

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,31 +1,37 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserTest {
 
-	
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://tanzeelalamacademy-fpcdgsgghraaf7ah.centralus-01.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("Tanzeelalamacademy.com Learning Tanzeel Alam"));
-		driver.close();
-	
-		
-		
-	}
+    @Test
+    public void getData() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://tanzeelalamacademy-fpcdgsgghraaf7ah.centralus-01.azurewebsites.net/webapp/");
+
+            String text = driver.findElement(By.cssSelector("h1")).getText();
+
+            System.out.println("Page Heading: " + text);
+
+            Assert.assertEquals(
+                    "Tanzeelalamacademy.com Learning Tanzeel Alam",
+                    text);
+
+        } finally {
+            driver.quit();
+        }
+    }
 }
